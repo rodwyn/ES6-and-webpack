cc899c8cfd1d072b972787772755bcdac1497452
Rodwyn Moreno <rodwynmoreno@gmail.com>
2017-02-15 10:34:21 -0600

    Added dev-server and babel features.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+console.log(__dirname);
 module.exports = {
   entry: ['./app/index.js'],
   output: {
-    path: './build',
+    path: __dirname + '/build',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    contentBase: __dirname + '/build',
+    inline: true
   }
 }
