1e41ee1d74e86ee7c36c3eb254ef5b03a434966b
Rodwyn Moreno <rodwynmoreno@gmail.com>
2017-02-16 13:09:12 -0600

    Added react app example.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,37 @@
-import { students, signatures } from './students';
-import { add, multiply } from './calculator';
-console.log(students, signatures);
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 
-console.log(add(3,5));
-console.log(multiply(3,5));
+const api_key = '3e738edcb82ed499e0adada8f8ff77e5';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: 'Mexico',
+      description: ''
+    }
+  }
+
+  componentDidMount() {
+    this.grabWeather(this.state.city);
+  }
+
+  grabWeather(city) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+      .then(response => response.json())
+      .then(json => {
+        this.setState({description: json.weather[0].description})
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Weather report for {this.state.city}</h1>
+        <h2>{this.state.description}</h2>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
--- app/students.js
+++
@@ -1,2 +0,0 @@
-export const students = ["Harry", "Ron", "Hermoine"];
-export const signatures = ["Math", "History", "philosophy"]
